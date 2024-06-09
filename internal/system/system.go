954e13c8604776f666c8ec05f6c7cb45d64b4a46
aforamitdev <aforamitrai@gmail.com>
2024-06-09 16:27:08 +0530

    added logger and contaxt

--- internal/system/system.go
+++ internal/system/system.go
@@ -4,13 +4,21 @@ import (
 	"context"
 
 	system_proto "github.com/aforamitdev/server-pilot/internal/proto/system"
+	"github.com/aforamitdev/server-pilot/pkg/logger"
 )
 
-type NewSystemInformer struct {
+type SystemInformer struct {
 	system_proto.UnimplementedInformerServer
+	log *logger.Logger
 }
 
-func (i NewSystemInformer) GetSystem(ctx context.Context, req *system_proto.SystemRequest) (*system_proto.SystemResponse, error) {
+func NewSystemInformer(log *logger.Logger) *SystemInformer {
+	return &SystemInformer{
+		log: log,
+	}
+}
+
+func (s SystemInformer) GetSystem(ctx context.Context, req *system_proto.SystemRequest) (*system_proto.SystemResponse, error) {
 	return &system_proto.SystemResponse{
 		Name: "Amti rai",
 	}, nil
